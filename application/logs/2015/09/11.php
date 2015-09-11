5a8e3a076577c9abde07d45807d0a6b7739f6d7c
Andrei <horoshavin-andr@mail.ru>
2015-09-11 08:52:44 +0300

    add kohana framework

---
+++ application/logs/2015/09/11.php
@@ -0,0 +1,32 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2015-09-11 00:34:48 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 158 ] in /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php:67
+2015-09-11 00:34:48 --- DEBUG: #0 /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('PHPSESSID', NULL)
+#1 /home/andrei/BusinessCard/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('PHPSESSID')
+#2 /home/andrei/BusinessCard/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php:67
+2015-09-11 00:34:53 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 158 ] in /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php:67
+2015-09-11 00:34:53 --- DEBUG: #0 /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('PHPSESSID', NULL)
+#1 /home/andrei/BusinessCard/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('PHPSESSID')
+#2 /home/andrei/BusinessCard/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php:67
+2015-09-11 00:35:22 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 158 ] in /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php:67
+2015-09-11 00:35:22 --- DEBUG: #0 /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('PHPSESSID', NULL)
+#1 /home/andrei/BusinessCard/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('PHPSESSID')
+#2 /home/andrei/BusinessCard/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php:67
+2015-09-11 00:36:40 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 158 ] in /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php:67
+2015-09-11 00:36:40 --- DEBUG: #0 /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('PHPSESSID', NULL)
+#1 /home/andrei/BusinessCard/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('PHPSESSID')
+#2 /home/andrei/BusinessCard/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php:67
+2015-09-11 00:36:40 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 158 ] in /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php:67
+2015-09-11 00:36:40 --- DEBUG: #0 /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('PHPSESSID', NULL)
+#1 /home/andrei/BusinessCard/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('PHPSESSID')
+#2 /home/andrei/BusinessCard/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php:67
+2015-09-11 00:36:41 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 158 ] in /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php:67
+2015-09-11 00:36:41 --- DEBUG: #0 /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('PHPSESSID', NULL)
+#1 /home/andrei/BusinessCard/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('PHPSESSID')
+#2 /home/andrei/BusinessCard/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/andrei/BusinessCard/system/classes/Kohana/Cookie.php:67
